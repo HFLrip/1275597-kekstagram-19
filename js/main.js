6993aa706bdc4d07b7f6bdad6f30c5f708c686fc
HFLrip <hflrip@mail.ru>
2020-01-28 20:16:53 +0300

    Переделал циклы в функции

--- js/main.js
+++ js/main.js
@@ -7,18 +7,25 @@ var photoDescriptions = [];
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+var x = getRandomNumber(0, 2);
 
-for (var j = 0; j < 6; j++) {
+var getComment = function () {
   var avatar = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
   var comment = {
     avatar: avatar,
     message: messages[getRandomNumber(0, 5)],
     name: namesOfCommentator[getRandomNumber(0, 3)],
   };
-  comments[j] = comment;
-}
-
-for (var i = 0; i < quntityOfDescriptions; i++) {
+  return comment;
+};
+var generatePhotoDescription = function () {
+  for (var b = 0; b < x; b++) {
+    if (x === 0) {
+      comments[b] = comments;
+    } else {
+      comments[b] = getComment();
+    }
+  }
   var url = 'photos/' + (i + 1) + '.jpg';
   var element = {
     url: url,
@@ -26,62 +33,28 @@ for (var i = 0; i < quntityOfDescriptions; i++) {
     likes: getRandomNumber(15, 200),
     comments: comments,
   };
-  photoDescriptions[i] = element;
+  return element;
+};
+for (var i = 0; i < quntityOfDescriptions; i++) {
+  photoDescriptions [i] = generatePhotoDescription();
 }
-
 var photoList = document.querySelector('.pictures'); // контейнер для фото по классу
 
 var template = document.querySelector('#picture').content;
 var pictureElement = template.querySelector('a');
-for (var n = 0; n < photoDescriptions.length; n++) {
-  var clonedPictureElement = pictureElement.cloneNode(true);
-  var quantityOfComment = clonedPictureElement.querySelector('.picture__comments');
-  quantityOfComment.textContent = photoDescriptions[n].comments;
-  var quantityOfLikes = clonedPictureElement.querySelector('.picture__likes');
-  quantityOfLikes.textContent = photoDescriptions[n].likes;
-  var imgSource = clonedPictureElement.querySelector('img');
-  imgSource.src = photoDescriptions[n].url;
-  photoList.appendChild(clonedPictureElement);
-}
-
-/* Вместо этого говна – клонирование ниже
-var makeElement = function (tagName, className, text) {
-  var elementOfPicture = document.createElement(tagName);
-  elementOfPicture.classList.add(className);
-  if (text) {
-    elementOfPicture.textContent = text;
+var pushDescriptionsInDom = function (photo) {
+  for (var n = 0; n < photo.length; n++) {
+    var clonedPictureElement = pictureElement.cloneNode(true);
+    var quantityOfComment = clonedPictureElement.querySelector('.picture__comments');
+    quantityOfComment.textContent = photo[n].comments;
+    var quantityOfLikes = clonedPictureElement.querySelector('.picture__likes');
+    quantityOfLikes.textContent = photo[n].likes;
+    var imgSource = clonedPictureElement.querySelector('img');
+    imgSource.src = photo[n].url;
+    photoList.appendChild(clonedPictureElement);
   }
-  return elementOfPicture;
 };
-var createPicture = function (photo) {
-  var listPicture = makeElement('a', 'picture');
-  var photoPicture = makeElement('img', 'picture__img');
-  photoPicture.src = photo.url;
-  photoPicture.alt = photo.description;
-  listPicture.appendChild(photoPicture);
-  var info = makeElement('p', 'picture__info');
-  listPicture.appendChild(info);
-  var pictureComments = makeElement('span', 'picture__comments', comments.length);
-  info.appendChild(pictureComments);
-  var pictureLikes = makeElement('span', 'picture__likes', photo.likes);
-  info.appendChild(pictureLikes);
-  return listPicture;
-};
-for (var n = 0; n < photoDescritpions.length; n++) {
-  var photoItem = createPicture(photoDescritpions[n]);
-  photoList.appendChild(photoItem);
-}
 
-Надо создать контейнер для фото по классу, затем добавить в него элемент
+pushDescriptionsInDom(photoDescriptions);
 
 
-  <template id="picture">
-    <a href="#" class="picture">
-      <img class="picture__img" src="" width="182" height="182" alt="Случайная фотография">
-      <p class="picture__info">
-        <span class="picture__comments"></span>
-        <span class="picture__likes"></span>
-      </p>
-    </a>
-  </template>
-  */
