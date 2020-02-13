5a87091de3f163d0122abaee42c76944bb21c1d6
HFLrip <hflrip@mail.ru>
2020-02-13 19:23:13 +0300

    убрал window.data в comments

--- js/picture.js
+++ js/picture.js
@@ -7,7 +7,7 @@
     for (var j = 0; j < photo.length; j++) {
       var clonedPictureElement = pictureElement.cloneNode(true);
       var quantityOfComment = clonedPictureElement.querySelector('.picture__comments');
-      quantityOfComment.textContent = photo[j].window.data.comments.length;
+      quantityOfComment.textContent = photo[j].comments.length;
       var quantityOfLikes = clonedPictureElement.querySelector('.picture__likes');
       quantityOfLikes.textContent = photo[j].likes;
       var imgSource = clonedPictureElement.querySelector('img');
