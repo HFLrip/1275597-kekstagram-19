ef300851dfb2e743a6b3e15103ad757c349cee18
HFLrip <hflrip@mail.ru>
2020-02-18 21:42:40 +0300

    добавил в превью отдельную функцию

--- js/preview.js
+++ js/preview.js
@@ -3,26 +3,25 @@
   var getBigPicture = function (photo) {
     var bigPicture = document.querySelector('.big-picture');
     bigPicture.classList.remove('hidden');
-    bigPicture.querySelector('.big-picture__img').src = photo[0].url;
-    bigPicture.querySelector('.likes-count').textContent = photo[0].likes;
-    bigPicture.querySelector('.comments-count').textContent = photo[0].comments.length;
-    bigPicture.querySelector('.social__caption').textContent = photo[0].description;
+    bigPicture.querySelector('.big-picture__img img').src = photo.url;
+    bigPicture.querySelector('.likes-count').textContent = photo.likes;
+    bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+    bigPicture.querySelector('.social__caption').textContent = photo.description;
     var commentList = document.querySelector('.social__comments');
-    for (var k = 0; k < photo[0].comments.length; k++) {
+    for (var k = 0; k < photo.comments.length; k++) {
       var commentItem = document.createElement('li');
       commentItem.classList.add('social__comment');
       commentList. appendChild(commentItem);
       var socialPicture = document.createElement('img');
       socialPicture.classList.add('social__picture');
-      socialPicture.src = photo[0].comments[k].avatar;
-      socialPicture.alt = photo[0].comments[k].name;
+      socialPicture.src = photo.comments[k].avatar;
+      socialPicture.alt = photo.comments[k].name;
       commentItem.appendChild(socialPicture);
       var textComment = document.createElement('p');
-      textComment.textContent = photo[0].comments[k].message;
+      textComment.textContent = photo.comments[k].message;
       commentItem.appendChild(textComment);
     }
   };
-  window.request(getBigPicture);
   window.preview = {
     getBigPicture: getBigPicture,
   };
